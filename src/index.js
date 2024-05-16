16c30e7fabb2ff55c0b8bea00d7fd5b9a4ad41c8
Ammar Lakis <ammar.lakis@gmail.com>
2024-05-16 07:28:17 +0200

    Support both js and yaml

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const addFormats = require("ajv-formats");
 const { promises: fs } = require("fs");
 const glob = require("glob-promise");
 const utils = require("@gh-actions-utils/inputs");
-const yaml = require("yaml")
+const jsyaml = require("js-yaml");
 
 async function loadData(pathOrData) {
   core.info(pathOrData);
@@ -13,7 +13,7 @@ async function loadData(pathOrData) {
     const data = await Promise.all(
       files.map((file) => fs.readFile(file, "utf8"))
     );
-    return data.map(yaml.parse);
+    return data.map(jsyaml.load);
   } catch (error) {
     core.setFailed(`Error loading data: ${error.message}`);
     throw error;
@@ -29,17 +29,13 @@ async function validate() {
 
     const ajv = new Ajv();
     addFormats(ajv);
-    core.info("new instance created");
-    core.info("schema is" + JSON.stringify(schemas) );
     const validate = ajv.compile(schemas[0]);
-    core.info("schema read");
-    core.info("data is " + JSON.stringify(data));
     const valid = validate(data[0]);
 
     if (!valid) {
       core.setFailed(`Validation errors: ${JSON.stringify(validate.errors)}`);
-      core.setOutput( "valid", false);
-      core.setOutput( "errors", JSON.stringify(validate.errors));
+      core.setOutput("valid", false);
+      core.setOutput("errors", JSON.stringify(validate.errors));
     } else {
       core.setOutput("valid", true);
     }
